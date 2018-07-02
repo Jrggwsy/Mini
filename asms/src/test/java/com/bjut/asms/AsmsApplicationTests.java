cb22cb64cc12074fc35dfd57b468d1467a6da335
Jrggwsy <40746070+Jrggwsy@users.noreply.github.com>
2018-07-02 20:55:19 +0800

    Add files via upload

---
+++ asms/src/test/java/com/bjut/asms/AsmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bjut.asms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
